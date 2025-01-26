e7c0170c49c5b96b751c818552a446035fd98457
geneciSI <genecicouto@id.uff.br>
2025-01-25 22:33:08 -0300

    Solução BAD versao 2

--- src/appsecao014fixacaofinal/Program.java
+++ src/appsecao014fixacaofinal/Program.java
@@ -26,14 +26,21 @@ public class Program {
 		
 		Account acc = new Account(number,holder, balance, withdrawLimit);
 		
-        System.out.println();
+		System.out.println();
         System.out.print("Informe uma quantia para sacar: ");
         double amount = sc.nextDouble();
-        
-        acc.withdraw(amount);
-        
-        System.out.printf("Novo saldo: %.2fn",acc.getBalance());
-        
+		
+		if (amount > acc.getWithdrawLimit()) {
+			System.out.println("Erro de saque: a quantia excede o limit");
+		}
+		else if (amount > acc.getBalance()) {
+			System.out.println("Erro de saque: saldo insuficiente");
+		}
+		else {
+			acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f%n",acc.getBalance());
+        }
+    
         sc.close();
 	}
 }
